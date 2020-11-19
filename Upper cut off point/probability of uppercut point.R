9480c91c6ca92a775a1a3e1355f88956706f164a
Dwilaksana Abdullah Rasyid <46968165+DwilaksanaAbdullahRasyid@users.noreply.github.com>
2020-11-19 20:35:56 +0800

    Delete probability of uppercut point.R

--- Upper cut off point/probability of uppercut point.R
+++
@@ -1,9 +0,0 @@
-#generate random normal standard
-rand_norm=rnorm(100000)
-boxplot(rand_norm)
-#calculate uppercut point
-uppercut=(quantile(rand_norm,0.75)+(1.5*(quantile(rand_norm,0.75)-quantile(rand_norm,0.25))))
-#calculate empirical CDF from random normal
-f_upper_cut=ecdf(rand_norm)
-#calculate probability of uppercut point
-prob=1-f_upper_cut(uppercut)
